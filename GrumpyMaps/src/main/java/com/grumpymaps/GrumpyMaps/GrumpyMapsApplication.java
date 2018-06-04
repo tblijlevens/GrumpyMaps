6717312546717c53350567bc86e618dc833c7457
Serge Bosman <bozman@live.nl>
2018-06-04 10:45:50 +0200

    Initial Commit

---
+++ GrumpyMaps/src/main/java/com/grumpymaps/GrumpyMaps/GrumpyMapsApplication.java
@@ -0,0 +1,12 @@
+package com.grumpymaps.GrumpyMaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrumpyMapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrumpyMapsApplication.class, args);
+	}
+}
---
+++ GrumpyMaps/src/main/java/com/grumpymaps/GrumpyMaps/model/Square.java
@@ -0,0 +1,5 @@
+package com.grumpymaps.GrumpyMaps.model;
+
+public class Square {
+
+}
