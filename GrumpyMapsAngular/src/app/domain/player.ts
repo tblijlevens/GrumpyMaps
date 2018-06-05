07afe6c12033372e73c3428dde8de62875aca7ab
Teun <tblijlevens@gmail.com>
2018-06-05 11:56:32 +0200

    implemented basic Player Class

--- GrumpyMapsAngular/src/app/domain/player.ts
+++ GrumpyMapsAngular/src/app/domain/player.ts
@@ -1,2 +1,20 @@
-export class Player {
+import {Physical} from './physical';
+
+export class Player implements Physical{
+
+    id:number;
+    name:string;
+    actionPoints:number;
+    movementAmount:number;
+    attacksPerRound:number;
+    spellsPerRound:number;
+
+    constructor(id, name, actionPoints, movementAmount, attacksPerRound, spellsPerRound){
+        this.id = id;
+        this.name = name;
+        this.actionPoints = actionPoints;
+        this.movementAmount = movementAmount;
+        this.attacksPerRound = attacksPerRound;
+        this.spellsPerRound = spellsPerRound;    
+    }
 }
