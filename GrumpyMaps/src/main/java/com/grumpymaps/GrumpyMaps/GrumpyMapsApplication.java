3eef8ae13a65c1019051660ac77bdc4f110de6ca
Teun <tblijlevens@gmail.com>
2018-07-12 11:52:16 +0200

    changed application file

--- GrumpyMaps/src/main/java/com/grumpymaps/GrumpyMaps/GrumpyMapsApplication.java
+++ GrumpyMaps/src/main/java/com/grumpymaps/GrumpyMaps/GrumpyMapsApplication.java
@@ -2,11 +2,20 @@ package com.grumpymaps.GrumpyMaps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
-public class GrumpyMapsApplication {
+public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GrumpyMapsApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
