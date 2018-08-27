3b7261094a8fe011bba8e9fe08e55a9f601018ae
Teun <tblijlevens@gmail.com>
2018-08-27 19:32:16 +0200

    start idea of checking dark colors and setting text to white if too dark

--- GrumpyMapsAngular/src/app/square/square.component.ts
+++ GrumpyMapsAngular/src/app/square/square.component.ts
@@ -260,6 +260,20 @@ export class SquareComponent implements OnInit {
       var isObstructed = this.square.obstructed;
       this.squareStyles['background-color'] = this.square.color;
 
+    //   // Assuming a six character colour:
+      //
+    //   var c = c.substring(1);      // strip #
+    //   var rgb = parseInt(c, 16);   // convert rrggbb to decimal
+    //   var r = (rgb >> 16) & 0xff;  // extract red
+    //   var g = (rgb >>  8) & 0xff;  // extract green
+    //   var b = (rgb >>  0) & 0xff;  // extract blue
+    //
+    //   var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
+    //
+    //   if (luma < 40) {
+    //       // make text white
+    //   }
+
         //   this.squareStyles['background'] = 'repeating-linear-gradient(          135deg, rgba(161, 0, 0, 0.6), rgba(161, 0, 0, 0.6) 8px, rgba(0, 0, 0, 0.0) 8px, rgba(0, 0, 0, 0.0) 16px)'
           //this.squareStyles['background'] = 'repeating-radial-gradient(circle, rgba(235, 199, 9, 0.6), rgba(235, 199, 9, 0.6) 5px, rgba(235, 199, 9, 0) 5px,  rgba(235, 199, 9, 0) 10px)'
     }
